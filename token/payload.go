63096cbc884cc31d76882e1ffaf9bdbeeb7d7e09
titus.dishon <titus.dishon@postcodelottery.co.uk>
2023-01-25 22:43:48 +0300

    implement jwt and paseto auth tokens

---
+++ token/payload.go
@@ -0,0 +1,41 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+var (
+	ErrorExpiredToken = errors.New("token has expired")
+	ErrorInvalidToken = errors.New("invalid token")
+)
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenId, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	payload := &Payload{
+		ID:        tokenId,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
+
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrorExpiredToken
+	}
+	return nil
+}
