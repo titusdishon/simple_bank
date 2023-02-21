c80459e8feca59a29500ca84b87019c443f123bc
titus.dishon <titus.dishon@postcodelottery.co.uk>
2023-02-21 23:14:39 +0300

    create user gRPC implementation

---
+++ gapi/converter.go
@@ -0,0 +1,18 @@
+package gapi
+
+import (
+	db "github.com/titusdishon/simple_bank/db/sqlc"
+	"github.com/titusdishon/simple_bank/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PhoneNumber:       user.PhoneNumber,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
